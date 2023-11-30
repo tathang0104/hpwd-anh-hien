05253b17cea4777bb209fc1133d14b112ff2b79a
tathang0104 <abcabc142001@gmail.com>
2023-12-01 04:30:43 +0700

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// function openVideoPopup() {
+//     document.getElementById('videoPopup').style.display = 'block';
+// }
+
+// function closeVideoPopup() {
+//     document.getElementById('videoPopup').style.display = 'none';
+// }
+
+const imgContainer = document.getElementById("image-container")
+const videoPopup = document.getElementById('videoPopup')
+const video = document.getElementById('hpwd-video')
+const closeBtn = document.getElementById('close-btn')
+const giftBox = imgContainer.getElementsByTagName("img")[0]
+
+imgContainer.addEventListener("click", () => {
+    giftBox.classList.add('image-enlarge');
+
+    setTimeout(function() {
+        giftBox.classList.remove('image-enlarge');
+    }, 400);
+
+    setTimeout(function() {
+        videoPopup.classList.add("show")
+        video.currentTime = 0
+        video.play()
+    }, 800);
+    
+    if (giftBox.src === window.location.origin + "/asset/gift-box.png") {
+        giftBox.src = window.location.origin + "/asset/gift-box-open.png"
+        giftBox.alt = "gift-box-open"
+        video.setAttribute("autoplay") = true
+    } else {
+        giftBox.src = window.location.origin + "/asset/gift-box.png"
+        giftBox.alt = "gift-box"
+        videoPopup.style.display = "none"
+    }
+})
+
+closeBtn.addEventListener("click", () => {
+    video.pause()
+    video.autoplay = false;
+    giftBox.src = window.location.origin + "/asset/gift-box.png"
+    giftBox.alt = "gift-box"
+    videoPopup.classList.remove("show")
+})
